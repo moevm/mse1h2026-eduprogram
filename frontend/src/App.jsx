ec49ce8dc7741edfed19b9fbe9a5c79fb2532a34
Kornienko Anton <antosha_kornienko1511@inbox.ru>
2026-03-14 17:31:13 +0300

    заглушка для страницы ввода рабочей программы

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import WorkProgramPage from './pages/WorkProgramPage';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/work_program" element={< WorkProgramPage/>} />
       </Routes>
     </BrowserRouter>
   );
