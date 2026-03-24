f40de0eca5ecb683c4bee7eb04bc58404f43037a
Dinar <khairullovdinarr@gmail.com>
2026-03-24 18:18:12 +0300

    render_graph

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage/LoginPage';
 import RegisterPage from './pages/RegisterPage/RegisterPage';
 import WorkProgramPage from './pages/WorkProgramPage/WorkProgramPage';
-import GraphPage from "./pages/GraphPage/GraphPage";
-import "./assets/styles/global.css"
+import GraphPage from './pages/GraphPage/GraphPage';
+import './assets/styles/global.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<GraphPage/>} />
+        <Route path="/" element={<LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/work_program" element={< WorkProgramPage/>} />
-        <Route path="/graph" element={<GraphPage/>} />
+        <Route path="/work_program" element={<WorkProgramPage />} />
+        <Route path="/graph" element={<GraphPage />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/components/GraphPanel/GraphPanel.jsx
+++ frontend/src/components/GraphPanel/GraphPanel.jsx
@@ -1,17 +1,50 @@
-import React from 'react';
-import GraphNavbar from "./GraphNavbar/GraphNavbar";
-import GraphAside from "./GraphAside/GraphAside";
-import GraphField from "./GraphField/GraphField";
-import "./GraphPanel.css"
+import React, { useRef, useState } from 'react';
+import GraphNavbar from './GraphNavbar/GraphNavbar';
+import GraphAside from './GraphAside/GraphAside';
+import GraphField from './GraphField/GraphField';
+import './GraphPanel.css';
 
-export default function GraphPanel() {
-    return (
-        <>
-            <GraphNavbar/>
-            <div className="graph-panel">
-                <GraphAside/>
-                <GraphField/>
-            </div>
-        </>
-    );
+const GraphPanel = ({ data }) => {
+  const graphFieldRef = useRef(null);
+  const [selectedNode, setSelectedNode] = useState(null);
+  const [selectedDirection, setSelectedDirection] = useState('Программная Инженерия');
+
+  const handleExportPNG = () => {
+    if (graphFieldRef.current) graphFieldRef.current.exportPNG();
+  };
+
+  const handleExportGraphPDF = () => {
+    if (graphFieldRef.current) graphFieldRef.current.exportPDF();
+  };
+
+  const handleNodeSelect = (nodeData) => {
+    setSelectedNode(nodeData);
+  };
+
+  if (!data) {
+    return <div className="graph-panel-empty">Данные графа не загружены</div>;
+  }
+
+  return (
+    <>
+      <GraphNavbar
+        onExportPNG={handleExportPNG}
+      />
+      <div className="graph-panel">
+        <GraphAside 
+          selectedNode={selectedNode}
+          selectedDirection={selectedDirection}
+          onDirectionChange={setSelectedDirection}
+          onExportGraphPDF={handleExportGraphPDF}
+        />
+        <GraphField
+          ref={graphFieldRef}
+          data={data}
+          onNodeSelect={handleNodeSelect}
+        />
+      </div>
+    </>
+  );
 };
+
+export default GraphPanel;
