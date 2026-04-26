ecc6305b95400f391f08e8981716d62a6bdcaadc
1nflutorm <1nflutorm@mail.ru>
2026-04-26 21:53:31 +0300

    minor fixes

--- backend/src/services/gigachat_service.py
+++ backend/src/services/gigachat_service.py
@@ -113,12 +113,13 @@ class GigachatService:
 
         if status_code == HTTPStatus.UNAUTHORIZED:
             self.get_access_token()
-        headers = {
-            'Authorization': f'Bearer {self.__access_token}',
-            'Content-Type': 'application/json'
-        }
-        response = requests.post(url, headers=headers, json=payload, verify=cert_path)
-        status_code = response.status_code
+            
+            headers = {
+                'Authorization': f'Bearer {self.__access_token}',
+            '   Content-Type': 'application/json'
+            }
+            response = requests.post(url, headers=headers, json=payload, verify=cert_path)
+            status_code = response.status_code
 
         try:
             response_data = response.json()
