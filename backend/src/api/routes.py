2ff3cc3a7cb5fdcd638e5b54491004f93902b706
Danila-gi <danilaivanovspb@gmail.com>
2026-05-24 00:52:18 +0300

    добавлено нахождение мостов + добавлены рекомендации к истории сравнения графа

--- backend/src/api/routes.py
+++ backend/src/api/routes.py
@@ -69,23 +69,31 @@ def getAvailableUniversities(db: DataBaseController = Depends(get_db)):
 @router.get("/get-available-export-formats")
 def getAvailableExportFormats():
     """Метод получения списка доступных форматов для экспорта."""
-    program_service = GraphService(None, None, None)
-    status_code, content = program_service.get_available_export_formats()
+    graph_service = GraphService(None, None, None)
+    status_code, content = graph_service.get_available_export_formats()
     return JSONResponse(status_code=status_code, content=content)
 
 @router.get("/download-graph")
 def downloadGraph(format: str, graphId: str, db: DataBaseController = Depends(get_db),
                              rdf: RdfController = Depends(get_rdf)):
     """Скачивание графа"""
-    program_service = GraphService(db, None, rdf)
-    response = program_service.get_export_file(format, graphId)
+    graph_service = GraphService(db, None, rdf)
+    response = graph_service.get_export_file(format, graphId)
     return response
+
+@router.get("/find-graph-bridges")
+def findGraphBridges(graphId: str, db: DataBaseController = Depends(get_db),
+                  rdf: RdfController = Depends(get_rdf)):
+    """Нахождение мостов графа"""
+    graph_service = GraphService(db, None, rdf)
+    status_code, content = graph_service.find_graph_bridges(graphId)
+    return JSONResponse(status_code=status_code, content=content)
 
 @router.get("/download-report")
 def downloadGraph(format: str, graphId: str, db: DataBaseController = Depends(get_db), rdf: RdfController = Depends(get_rdf)):
     """Скачивание отчета"""
-    program_service = GraphService(db, None, rdf)
-    response = program_service.get_report_file(format, graphId)
+    graph_service = GraphService(db, None, rdf)
+    response = graph_service.get_report_file(format, graphId)
     return response
 
 @router.get("/get-history")
@@ -96,9 +104,10 @@ def getHistory(user_id: int, db: DataBaseController = Depends(get_db)):
     return JSONResponse(status_code=status_code, content=content)
 
 @router.get("/get-history-graph")
-def getHistoryGraph(user_id: int, hash: str, rdf: RdfController = Depends(get_rdf)):
+def getHistoryGraph(user_id: int, hash: str, db: DataBaseController = Depends(get_db),
+                    rdf: RdfController = Depends(get_rdf)):
     """Метод истории сравнений."""
-    program_service = ProgramService(None, rdf)
+    program_service = ProgramService(db, rdf)
     status_code, content = program_service.get_history_graph(user_id, hash)
     return JSONResponse(status_code=status_code, content=content)
 
